e4010704c433db906638699d7070e7a1ae83b605
absentees <scottblissett@gmail.com>
2016-11-18 12:02:21 +1100

    Caspar token

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
 router.use(function(req, res, next){
-	if (req.body.token == process.env.slack_token || req.body.token == process.env.slack_token_2 || req.body.token == process.env.slack_token_3 || req.body.token == process.env.slack_token_4 ) {
+	if (req.body.token == process.env.slack_token || req.body.token == process.env.slack_token_2 || req.body.token == process.env.slack_token_3 || req.body.token == process.env.slack_token_4  || req.body.token == process.env.slack_token_5) {
 		// Request has come from slack, all good
 		console.log("TOKEN MATCHES");
 		next();
