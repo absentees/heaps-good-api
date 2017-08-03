eae05090fb0d69af632b3babac2b4d8c6ed89450
absentees <scottblissett@gmail.com>
2017-08-03 17:20:10 +1000

    remove warne

--- app.js
+++ app.js
@@ -48,10 +48,6 @@ router.route('/caspart').post(function(res,req) {
 	require('./caspart/').init(res,req);
 });
 
-router.route('/warne-wisdom').post(function(res,req) {
-	require('./warne-wisdom/').init(res,req);
-});
-
 router.route('/it').post(function(res,req) {
 	require('./it/').init(res,req);
 });
